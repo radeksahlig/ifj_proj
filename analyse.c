a0166b0d6861e808934bc853b6c9c31b5df83640
Methack <45518811+Methack@users.noreply.github.com>
2019-11-30 03:25:31 +0100

    Update analyse.c

--- analyse.c
+++ analyse.c
@@ -71,53 +71,12 @@ static int arg();
 static int arg_n();
 int precedent_analys();
 
-int new_token(Token *token){
-	int retval= get_token(token);
-	switch(token->type){
-	    case TOKEN_EMPTY_FILE : printf("token_empty \n");break;
-	    case TOKEN_KEYWORD : printf("token_keyword \n");break;
-	    case TOKEN_ID :printf("token_id \n");break;
-	    case TOKEN_INTEGER : printf("token_int \n");break;
-	    case TOKEN_FLOAT : printf("token_float \n");break;
-	    case TOKEN_STRING : printf("token_string \n");break;
-	    case TOKEN_EOF : printf("token_eof \n");break;
-	    case TOKEN_EOL : printf("token_eol \n");break;
-	    case TOKEN_COMMA : printf("token_comma \n");break;
-	    case TOKEN_L_BRACKET : printf("token_( \n");break;
-	    case TOKEN_R_BRACKET : printf("token_) \n");break;
-	    case TOKEN_PLUS : printf("token_+ \n");break;
-	    case TOKEN_MINUS : printf("token_- \n");break;
-	    case TOKEN_MUL : printf("token_* \n");break;
-	    case TOKEN_FLOAT_DIV : printf("token_/ \n");break;
-	    case TOKEN_INT_DIV : printf("token_// \n");break;
-	    case TOKEN_MEQ : printf("token_>= \n");break;
-	    case TOKEN_MORE : printf("token_> \n");break;
-	    case TOKEN_LEQ : printf("token_<= \n");break;
-	    case TOKEN_LESS : printf("token_< \n");break;
-	    case TOKEN_EQ : printf("token_== \n");break;
-	    case TOKEN_COLON : printf("token_: \n");break;
-	    case TOKEN_ASSIGN : printf("token_= \n");break;
-	    case TOKEN_NEQ : printf("token_!= \n");break;
-	    case TOKEN_INDENT : printf("token_indent --------------->\n");break;
-	    case TOKEN_DEDENT : printf("token_dedent <---------------\n");break;
-	}
-	if(token->type == TOKEN_STRING){
-		printf(" ## %s ##", token->attribute.string->string);	
-	}
-	if(retval != 0){
-		printf("%d return new_token\n", retval);
-		return retval;}
-	return 0;
-}
-int i, j;
 int prog(){
-	printf("---in prog\n");
 	int retval;
     	int cur_indent = 0;
     	int prev_indent = 0;
 	if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_DEF){
         //1. <prog> -> def id (<params>) : EOL <stmt> EOL <prog>
-	printf("---in def\n");
         GET_TOKEN_CHECK_TYPE(TOKEN_ID);
         //proměnná
         GET_TOKEN_CHECK_TYPE(TOKEN_L_BRACKET);
@@ -131,16 +90,13 @@ int prog(){
         GET_TOKEN_CHECK_TYPE(TOKEN_EOL);
         return prog();
 	}else if(token.type == TOKEN_EOL){
-	printf("---in eol prog\n");
         //2. <prog> -> EOL <prog>
 	GET_TOKEN();
         return prog();
 	}else if(token.type == TOKEN_EOF){
-	printf("---in eof\n");
         //3. <prog> -> EOF
         return SYNTAX_OK;
-	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_IF){
-	printf("---in if prog\n");        
+	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_IF){    
 	//4. <prog> -> if <expr> : EOL <stmt> else : EOL <stmt> <prog>
         GET_TOKEN_CHECK_RULE(precedent_analys);
         GET_TOKEN_CHECK_TYPE(TOKEN_COLON);
@@ -148,7 +104,6 @@ int prog(){
         CHECK_NUM_INDENT(cur_indent, prev_indent);
         CHECK_STMT(prev_indent, cur_indent);
 	cur_indent = 0;
-	printf("---in else prog\n");
 	if(token.type != TOKEN_KEYWORD || token.attribute.keyword != KEYWORD_ELSE)
 		return SYNTAX_ERROR;  
         //GET_TOKEN_CHECK_KEYWORD(KEYWORD_ELSE);
@@ -159,7 +114,6 @@ int prog(){
 	cur_indent = 0;
         return prog();
 	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_WHILE){
-	printf("---in while prog\n");
         //5. <prog> -> while <expr> : EOL <stmt> EOL <prog>
         GET_TOKEN_CHECK_RULE(precedent_analys);
         GET_TOKEN_CHECK_TYPE(TOKEN_COLON);
@@ -169,14 +123,11 @@ int prog(){
 	cur_indent = 0;
         return prog();
 	}else if(token.type == TOKEN_ID){
-	printf("---in id prog\n");
         //6. <prog> -> id<def> EOL <prog>
         GET_TOKEN_CHECK_RULE(def);
-	printf("prošlo id ---\n");
         GET_TOKEN_CHECK_TYPE(TOKEN_EOL);
         return prog();
-	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_PASS){
-	printf("---in pass prog\n");        
+	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_PASS){   
 	//7. <prog> -> pass EOL <prog>
         GET_TOKEN_CHECK_TYPE(TOKEN_EOL);
         return prog();
@@ -190,17 +141,14 @@ int prog(){
 static int stmt(int prev_indent, int cur_indent){
 	int retval;
 	int this_indent = cur_indent;
-	printf("---in stmt indent : %d - %d token\n", prev_indent, cur_indent);
 	if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_IF){
         //8. <stmt> ->  if <expr> : EOL <stmt> else : EOL <stmt> EOL <stmt>
-	printf("---in if stmt\n"); 
         GET_TOKEN_CHECK_RULE(precedent_analys);
         GET_TOKEN_CHECK_TYPE(TOKEN_COLON);
         GET_TOKEN_CHECK_TYPE(TOKEN_EOL);
         CHECK_NUM_INDENT(cur_indent, this_indent);
         CHECK_STMT(this_indent, cur_indent);
 	cur_indent = this_indent;
-	printf("---in else stmt\n");
 	if(token.type != TOKEN_KEYWORD || token.attribute.keyword != KEYWORD_ELSE)
 		return SYNTAX_ERROR;  
         //GET_TOKEN_CHECK_KEYWORD(KEYWORD_ELSE);
@@ -211,7 +159,6 @@ static int stmt(int prev_indent, int cur_indent){
 	cur_indent = this_indent;
         return stmt(prev_indent, cur_indent);
 	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_WHILE){
-	printf("---in while stmt\n"); 
         //9. <stmt> ->  while <expr> : EOL <stmt> EOL <stmt>
         GET_TOKEN_CHECK_RULE(precedent_analys);
         GET_TOKEN_CHECK_TYPE(TOKEN_COLON);
@@ -221,32 +168,27 @@ static int stmt(int prev_indent, int cur_indent){
 	cur_indent = this_indent;
         return stmt(prev_indent, cur_indent);
 	}else if(token.type == TOKEN_ID){
-	printf("---in id stmt\n"); 
         //11. <stmt> -> id <def> EOL <stmt> 
         GET_TOKEN_CHECK_RULE(def);
         GET_TOKEN_CHECK_TYPE(TOKEN_EOL);
         GET_TOKEN();
         return stmt(prev_indent, cur_indent);
-	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_PASS){
-	printf("---in pass stmt\n");         
+	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_PASS){  
 	//13. <stmt> -> pass EOL <stmt>
        GET_TOKEN_CHECK_TYPE(TOKEN_EOL);
        GET_TOKEN();
        return stmt(prev_indent, cur_indent);
 	}else if(token.type == TOKEN_EOL){
-	printf("---in eol stmt\n"); 
         //10. <stmt> -> EOL <stmt>
         GET_TOKEN();
         return stmt(prev_indent, cur_indent);
 	}else if(token.type == TOKEN_KEYWORD && token.attribute.keyword == KEYWORD_RETURN){
-	printf("---in return stmt\n"); 
         //12. <stmt> -> return <expr> EOL <stmt>
         GET_TOKEN_CHECK_RULE(precedent_analys);
         GET_TOKEN_CHECK_TYPE(TOKEN_EOL);
         CHECK_NUM_DEDENT(cur_indent, prev_indent);
 	}
 	if(token.type == TOKEN_DEDENT){
-	printf("---in dedent stmt %d - %d token\n", prev_indent, cur_indent);
         CHECK_NUM_DEDENT(cur_indent, prev_indent);
     }
 	printf("---in stmt end\n"); 
@@ -255,7 +197,6 @@ static int stmt(int prev_indent, int cur_indent){
 }
 
 static int params(){
-	printf("---in params\n");
 	if(token.type == TOKEN_ID){
         //16. <params> -> id <param_n>;
         //nějaká kontrola
@@ -267,7 +208,6 @@ static int params(){
 
 static int param_n(){
 	int retval;
-	printf("---in param_n\n");
 	if(token.type == TOKEN_COMMA){
         //18. <param_n> -> , id <param_n>
         GET_TOKEN_CHECK_TYPE(TOKEN_ID);
@@ -278,7 +218,6 @@ static int param_n(){
 
 static int def(){
 	int retval;
-	printf("---in def\n");
 	if(token.type == TOKEN_L_BRACKET){
         //19. <def> -> (<arg>)
         GET_TOKEN_CHECK_RULE(arg);
@@ -296,7 +235,6 @@ static int def(){
 
 static int rovn(){
 	int retval;
-	printf("---in rovn\n");
     if(token.type == TOKEN_ID){
         //23. <rovn> -> id(<arg>)
         //kontrola proměnné
@@ -314,13 +252,11 @@ static int rovn(){
 }
 
 static int value(){
-	printf("---in value\n");
     if(token.type == TOKEN_INTEGER){
         //24. <value> -> int_value
         return SYNTAX_OK;
     }else if(token.type == TOKEN_STRING){
         //26. <value> -> str_value
-	printf("value string\n");
         return SYNTAX_OK;
     }else if(token.type == TOKEN_FLOAT){
         //25. <value> -> float_value
@@ -334,7 +270,6 @@ static int value(){
 }
 
 static int arg(){
-	printf("---in arg\n");
         
     //29. <arg> -> <value> <arg_n>
 	if(token.type == TOKEN_ID || token.type == TOKEN_FLOAT || token.type == TOKEN_STRING || token.type == TOKEN_INTEGER){
@@ -343,7 +278,6 @@ static int arg(){
 			return retval;
 		}
 	    	GET_TOKEN();
-		printf("---in arg %d\n", i);	
 	    	return arg_n();
 	}else if(token.type == TOKEN_R_BRACKET){
 		//28. <arg> -> ε
@@ -354,7 +288,6 @@ static int arg(){
 
 static int arg_n(){
 	int retval;
-	printf("---in arg_n %d\n", i);
     if(token.type == TOKEN_COMMA){
         //31. <arg_n> -> , <value> <arg-n>
         GET_TOKEN_CHECK_RULE(value);
@@ -368,7 +301,6 @@ static int arg_n(){
 }
 
 int precedent_analys(){
-	printf("---in expr\n");
 	return SYNTAX_OK;
 }
 
@@ -385,8 +317,7 @@ int main(){
 	
 
 	FILE* file;	
-	if((file = fopen("./code/test1", "r")))
-		printf("Soubor otevřen");
+	file = stdin;
 	set_file(file);
 	GET_TOKEN();
 	int cont = prog();
